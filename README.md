4a88dcb7fbe6664cd624f39b930532422a9c33fe
Mathijs Vogelzang <mathijs@gmail.com>
2022-03-18 15:47:23 +0100

    Boilerplate vite/vue setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Lens hack
+
+Submission for LFGrow hackathon
+
+## Run locally
+
+Pull the repository, run 
+```
+pnpm i
+pnpm dev
+```
+
+The site is then available at localhost:3000
+
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="container app position-relative">
+    <h1>Lens hack</h1>
+    <h5>LFGrow 2022</h5>
+  </div>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { computed, ref, watch } from 'vue';
+
+</script>
+
+<style>
+.app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
